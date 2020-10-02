b0d9724e3e4fe893760d34d8fce9b3c82dff44ed
Joseph Oppegaard <joe@montanab.com>
2020-10-01 23:00:09 -0700

    add in name

--- inside_dread_marshal_stance_aura.js
+++ inside_dread_marshal_stance_aura.js
@@ -1,6 +1,8 @@
 (async () => {
 	let messageContent = '';
 	if (actor) {
+		let name = token.actor.data.name;
+
 		if ((actor.data.data.customModifiers['damage'] || []).some(modifier => modifier.name === 'Inside Dread Marshal Stance Aura')) {
 			await actor.removeCustomModifier('damage', 'Inside Dread Marshal Stance Aura');
 			
@@ -8,17 +10,17 @@
 				await token.toggleEffect("systems/pf2e/icons/conditions-3/hostile.png")
 			}
 
-			messageContent = 'Is no longer Inside Dread Marshal Stance Aura.'
+			messageContent = name + ' is no longer Inside Dread Marshal Stance Aura.'
 
 		}
 		else {
-			await actor.addCustomModifier('damage', 'Inside Dread Marshal Stance Aura', 2, 'status');
+			await actor.addCustomModifier('damage', 'Inside Dread Marshal Stance Aura', 3, 'status');
 
 			if (!token.data.effects.includes("systems/pf2e/icons/conditions-3/hostile.png")) {
 				await token.toggleEffect("systems/pf2e/icons/conditions-3/hostile.png")
 			}
 
-			messageContent = 'Is Inside Dread Marshal Stance Aura (+2 status bonus to damage).'
+			messageContent = name + ' is Inside Dread Marshal Stance Aura (+3 status bonus to damage).'
 		};
 
 		if (messageContent !== '') {
